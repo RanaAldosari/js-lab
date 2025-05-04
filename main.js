17c554513c13339895b5a28cc88e5a570effc75b
rno9i <iirn8944@gmail.com>
2025-05-04 10:59:48 +0300

    done

---
+++ main.js
@@ -0,0 +1,52 @@
+// 1
+let height=200;
+let width=150;
+let reqtangul=height+width;
+console.log("The area of the rectangle is:",reqtangul)
+
+// 2
+let celiseis=30;
+let fehrenhait=(celiseis*9/5)+30;
+console.log(celiseis + "NN°C is"+fehrenhait+"NN°F");
+
+fehrenhait=84;
+celiseis=(fehrenhait-32)*5/9
+console.log(fehrenhait+"NN°F is"+celiseis.toFixed(2)+"NN°C.")
+
+// 3
+let hours=4;
+let seconds=hours*60*60*60*60;
+console.log(hours+" hourse",seconds+" seconds")
+
+// 4
+/*
+ * REQUIREMENTS
+ * 1. Your code should have two variables -  `thingOne` and `thingTwo`
+ * 2. Your code should only have one `console.log` statement
+ * 3. Your code should print out the values of `thingOne` and `thingTwo` using concatenation
+ * 4. Your code should contain semicolons at the end of each line
+ */
+let thingone="welcome";
+let thingtwo="Rana";
+console.log(thingone+thingtwo);
+
+// 5
+let bill=10.25 + 3.99 + 7.15;
+let tip=bill*0.5;
+let total=bill+tip;
+console.log("Total is:",total)
+
+// 6
+let adjective1="Amazing";
+let adjective2="fun";
+let adjective3="entertaining";
+let madlip="The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madlip)
+
+// 7
+let firstName = "Julia";
+let interest = "cats";
+let  hobby = "play video games";
+let awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage);
+
